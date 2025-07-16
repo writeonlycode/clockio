6ba319ff9a3a4c19adaa9a9a0c7b2ba0d8343e32
Iago Bozza <iagobozza@writeonlycode.io>
2025-07-16 12:28:30 -0300

    Remove sleep from Sign In Page

--- src/app/(pages)/@modal/(.)sign-in/page.tsx
+++ src/app/(pages)/@modal/(.)sign-in/page.tsx
@@ -1,7 +1,5 @@
 import { SignInForm } from "@/components/auth/sign-in-form";
-import { sleep } from "@/lib/utils";
 
 export default async function SignUpPage() {
-  await sleep(3000);
   return <SignInForm />;
 }
